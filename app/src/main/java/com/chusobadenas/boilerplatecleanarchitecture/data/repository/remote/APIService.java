6812f35ee4cfee9294c3d94c7114269d4d28cc3e
Jesús Badenas <chusobadenas@gmail.com>
2017-11-29 17:55:10 +0100

    Update data package

--- app/src/main/java/com/chusobadenas/boilerplatecleanarchitecture/data/repository/remote/APIService.java
+++ app/src/main/java/com/chusobadenas/boilerplatecleanarchitecture/data/repository/remote/APIService.java
@@ -1,5 +1,6 @@
 package com.chusobadenas.boilerplatecleanarchitecture.data.repository.remote;
 
+import com.chusobadenas.boilerplatecleanarchitecture.BuildConfig;
 import com.chusobadenas.boilerplatecleanarchitecture.data.entity.UserEntity;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -7,61 +8,74 @@ import com.google.gson.GsonBuilder;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import io.reactivex.Observable;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
-import rx.Observable;
 
 /**
  * APIService for retrieving data from the network using Retrofit 2
  */
 public interface APIService {
 
-    String API_BASE_URL = "http://www.android10.org/";
-    String API_VERSION = "/myapi";
-    String EXTENSION = ".json";
-    String USER_ID = "userId";
+  int CONNECT_TIMEOUT = 15000;
+  int READ_TIMEOUT = 20000;
+  int WRITE_TIMEOUT = 20000;
 
-    /**
-     * Retrieves an {@link rx.Observable} which will emit a List of {@link UserEntity}.
-     */
-    @GET(API_VERSION + "/users" + EXTENSION)
-    Observable<List<UserEntity>> userEntityList();
+  String API_BASE_URL = "https://raw.githubusercontent.com/";
+  String USER_ID = "userId";
 
-    /**
-     * Retrieves an {@link rx.Observable} which will emit a {@link UserEntity}.
-     *
-     * @param userId The user id used to get user data.
-     */
-    @GET(API_VERSION + "/user_{" + USER_ID + "}" + EXTENSION)
-    Observable<UserEntity> userEntityById(@Path(USER_ID) int userId);
+  /**
+   * Retrieves an {@link Observable} which will emit a List of {@link UserEntity}.
+   */
+  @GET("/android10/Sample-Data/master/Android-CleanArchitecture/users.json")
+  Observable<List<UserEntity>> userEntityList();
 
-    /********
-     * Helper class that sets up a new services
-     *******/
-    class Creator {
+  /**
+   * Retrieves an {@link Observable} which will emit a {@link UserEntity}.
+   *
+   * @param userId The user id used to get user data.
+   */
+  @GET("/android10/Sample-Data/master/Android-CleanArchitecture/user_{" + USER_ID + "}.json")
+  Observable<UserEntity> userEntityById(@Path(USER_ID) int userId);
 
-        private static OkHttpClient createHttpClient() {
-            return new OkHttpClient.Builder()
-                    .readTimeout(10000, TimeUnit.MILLISECONDS)
-                    .connectTimeout(15000, TimeUnit.MILLISECONDS)
-                    .build();
-        }
+  /********
+   * Helper class that sets up a new services
+   *******/
+  class Creator {
 
-        public static APIService newAPIService() {
-            Gson gson = new GsonBuilder()
-                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
-                    .create();
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(API_BASE_URL)
-                    .client(Creator.createHttpClient())
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                    .build();
-            return retrofit.create(APIService.class);
-        }
+    private static OkHttpClient createHttpClient() {
+      OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
+
+      // Enable logging
+      if (BuildConfig.DEBUG) {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        clientBuilder.addInterceptor(interceptor);
+      }
+
+      return clientBuilder
+          .connectTimeout(CONNECT_TIMEOUT, TimeUnit.MILLISECONDS)
+          .readTimeout(READ_TIMEOUT, TimeUnit.MILLISECONDS)
+          .writeTimeout(WRITE_TIMEOUT, TimeUnit.MILLISECONDS)
+          .build();
     }
+
+    public static APIService newAPIService() {
+      Gson gson = new GsonBuilder()
+          .setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+          .create();
+      Retrofit retrofit = new Retrofit.Builder()
+          .baseUrl(API_BASE_URL)
+          .client(Creator.createHttpClient())
+          .addConverterFactory(GsonConverterFactory.create(gson))
+          .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+          .build();
+      return retrofit.create(APIService.class);
+    }
+  }
 }
--- app/src/test/java/com/chusobadenas/boilerplatecleanarchitecture/data/entity/mapper/UserEntityDataMapperTest.java
+++ app/src/test/java/com/chusobadenas/boilerplatecleanarchitecture/data/entity/mapper/UserEntityDataMapperTest.java
@@ -15,64 +15,64 @@ import static org.junit.Assert.assertSame;
 
 public class UserEntityDataMapperTest {
 
-    private static final String COVER_URL = "coverUrl";
-    private static final String DESCRIPTION = "description";
-    private static final String EMAIL = "email";
-    private static final String FULL_NAME = "fullName";
+  private static final String COVER_URL = "coverUrl";
+  private static final String DESCRIPTION = "description";
+  private static final String EMAIL = "email";
+  private static final String FULL_NAME = "fullName";
 
-    private static final int FOLLOWERS = 10;
-    private static final int USER_ID = 1;
+  private static final int FOLLOWERS = 10;
+  private static final int USER_ID = 1;
 
-    private UserEntityDataMapper mUserEntityDataMapper;
+  private UserEntityDataMapper userEntityDataMapper;
 
-    @Before
-    public void setUp() {
-        mUserEntityDataMapper = new UserEntityDataMapper();
-    }
+  @Before
+  public void setUp() {
+    userEntityDataMapper = new UserEntityDataMapper();
+  }
 
-    @Test
-    public void testTransformToUser() {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setCoverUrl(COVER_URL);
-        userEntity.setDescription(DESCRIPTION);
-        userEntity.setEmail(EMAIL);
-        userEntity.setFollowers(FOLLOWERS);
-        userEntity.setFullname(FULL_NAME);
-        userEntity.setUserId(USER_ID);
+  @Test
+  public void testTransformToUser() {
+    UserEntity userEntity = new UserEntity();
+    userEntity.setCoverUrl(COVER_URL);
+    userEntity.setDescription(DESCRIPTION);
+    userEntity.setEmail(EMAIL);
+    userEntity.setFollowers(FOLLOWERS);
+    userEntity.setFullname(FULL_NAME);
+    userEntity.setUserId(USER_ID);
 
-        User user = mUserEntityDataMapper.transform(userEntity);
+    User user = userEntityDataMapper.transform(userEntity);
 
-        assertEquals(user.getCoverUrl(), COVER_URL);
-        assertEquals(user.getDescription(), DESCRIPTION);
-        assertEquals(user.getEmail(), EMAIL);
-        assertSame(user.getFollowers(), FOLLOWERS);
-        assertEquals(user.getFullName(), FULL_NAME);
-        assertSame(user.getUserId(), USER_ID);
-    }
+    assertEquals(user.getCoverUrl(), COVER_URL);
+    assertEquals(user.getDescription(), DESCRIPTION);
+    assertEquals(user.getEmail(), EMAIL);
+    assertSame(user.getFollowers(), FOLLOWERS);
+    assertEquals(user.getFullName(), FULL_NAME);
+    assertSame(user.getUserId(), USER_ID);
+  }
 
-    @Test
-    public void testTransformToUserCollection() {
-        List<UserEntity> userEntities = new ArrayList<>();
-        UserEntity userEntity = new UserEntity();
-        userEntity.setCoverUrl(COVER_URL);
-        userEntity.setDescription(DESCRIPTION);
-        userEntity.setEmail(EMAIL);
-        userEntity.setFollowers(FOLLOWERS);
-        userEntity.setFullname(FULL_NAME);
-        userEntity.setUserId(USER_ID);
-        userEntities.add(userEntity);
+  @Test
+  public void testTransformToUserCollection() {
+    List<UserEntity> userEntities = new ArrayList<>();
+    UserEntity userEntity = new UserEntity();
+    userEntity.setCoverUrl(COVER_URL);
+    userEntity.setDescription(DESCRIPTION);
+    userEntity.setEmail(EMAIL);
+    userEntity.setFollowers(FOLLOWERS);
+    userEntity.setFullname(FULL_NAME);
+    userEntity.setUserId(USER_ID);
+    userEntities.add(userEntity);
 
-        List<User> users = mUserEntityDataMapper.transform(userEntities);
+    List<User> users = userEntityDataMapper.transform(userEntities);
 
-        assertFalse(users.isEmpty());
-        assertSame(users.size(), 1);
+    assertFalse(users.isEmpty());
+    assertSame(users.size(), 1);
 
-        User user = users.get(0);
-        assertEquals(user.getCoverUrl(), COVER_URL);
-        assertEquals(user.getDescription(), DESCRIPTION);
-        assertEquals(user.getEmail(), EMAIL);
-        assertSame(user.getFollowers(), FOLLOWERS);
-        assertEquals(user.getFullName(), FULL_NAME);
-        assertSame(user.getUserId(), USER_ID);
-    }
+    User user = users.get(0);
+    assertEquals(user.getCoverUrl(), COVER_URL);
+    assertEquals(user.getDescription(), DESCRIPTION);
+    assertEquals(user.getEmail(), EMAIL);
+    assertSame(user.getFollowers(), FOLLOWERS);
+    assertEquals(user.getFullName(), FULL_NAME);
+    assertSame(user.getUserId(), USER_ID);
+  }
 }
